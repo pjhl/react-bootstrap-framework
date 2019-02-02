209a8beee844555628ed8f0ac004542451f1855c
Pjhl <seregal2009@yandex.ru>
2019-02-02 23:04:45 +0200

    Started work on inputs

---
+++ packages/rbf-formatter/src/components/input/Input.js
@@ -0,0 +1,7 @@
+import { Component } from 'react'
+
+export default class Input extends Component {
+  render () {
+    throw new Error('Method render() of class "Input" should be redefined.')
+  }
+}
---
+++ packages/rbf-formatter/src/components/input/TextInput.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Input from './Input'
+import PropTypes from 'prop-types'
+
+export default class TextInput extends Input {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    attribute: PropTypes.string,
+    value: PropTypes.string,
+    record: PropTypes.object,
+    onChange: PropTypes.func
+  }
+  static defaultProps = {
+    name: '',
+    attribute: '',
+    value: '',
+    record: {},
+    onChange: (value, attribute, record) => {}
+  }
+  onChange = (e) => {
+    const value = e.target.value
+    const { attribute, record } = this.props
+    this.props.onChange(value, attribute, record)
+  }
+  render () {
+    const { value } = this.props
+    return (
+      <input
+        type='text'
+        value={value}
+        onChange={this.onChange} />
+    )
+  }
+}
